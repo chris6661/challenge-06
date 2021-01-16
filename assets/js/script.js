58925aae865fe22045365b019e761712bf5e9cec
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-01-16 16:48:09 -0600

    set up html page with bootstrap and listed out tasks that need completed in javascript

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// USER STORY
+
+// AS A traveler
+// I WANT to see the weather outlook for multiple cities
+// SO THAT I can plan a trip accordingly
+
+// YOU NEED TO:
+// -make search bar for city after you put grid from bootstrap ***extra fun: add auto fill for city, refer back to js lessons you did***
+// -show current forecast for the day as well as future, 5 day forecast
+// -when weather conditions are clicked onemptied, show city name, date, icon representation for conditions (sunny, cloudy, etc.), temperature, humidity, wind ScopedCredential, and uv index (review current weather sites for accurate depiction of uv index display)
+// -when uv index is clicked on, a color should pop up for favorable, moderate, or severe weather
+// -when a city in the past searches is clicked on, data should return for that individual city repeating above conditions
+
+//NEED TO GET API KEY
+//api.openweathermap.org/data/2.5/weather?q=(city name)&APPID=5387c985f4e91ddb7edd648ae64e08cc
+
